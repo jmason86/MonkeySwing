281a5759871a61961241f41c97cdbce423261511
James Paul Mason <jmason86@Yoshi.local>
2014-01-10 18:26:47 -0500

    It’s now possible to swipe to make the monkey swing and tap to make the monkey release the rope. On release, the contact mask for the current rope is set to 0 so the monkey can’t rejoin with it. Other ropes are not affected so contacting those causes a new joint to be formed between the monkey and the rope segment.

--- MonkeySwing/MyScene.m
+++ MonkeySwing/MyScene.m
@@ -8,8 +8,10 @@
 
 #import "MyScene.h"
 
-static const CGFloat k_swipeToXVelocityConversion = 0.02;
-static const CGFloat k_swipeToYVelocityConversion = 0.01;
+static const CGFloat k_swipeToXVelocityConversion = 0.4;
+static const CGFloat k_swipeToYVelocityConversion = 0.2;
+static const uint32_t monkeyCategory =  0x1 << 0;
+static const uint32_t ropeCategory =  0x1 << 1;
 
 @implementation MyScene
 {
@@ -17,6 +19,10 @@ static const CGFloat k_swipeToYVelocityConversion = 0.01;
     CGFloat sceneWidth, sceneHeight;
     NSTimeInterval touchBeganTime;
     CGPoint touchBeganPoint;
+    int treeDensity; // [trees/screen]
+    int bushDensity; // [bushes/screen]
+    NSTimer *fireTimer;
+    NSString *monkeyOnRopeWithName;
 }
 
 -(id)initWithSize:(CGSize)size {    
@@ -25,6 +31,8 @@ static const CGFloat k_swipeToYVelocityConversion = 0.01;
         [self defineUsefulConstants];
         
         // Add background images (sky and forest)
+        treeDensity = 7; // [trees/screen]
+        bushDensity = 15; // [bushes/screen]
         [self addBackground];
         
         // Add fire
@@ -34,16 +42,17 @@ static const CGFloat k_swipeToYVelocityConversion = 0.01;
         [self addRopes];
         
         // Add monkey
+        [self addMonkey];
         
+        // Setup physicsWorld
+        self.physicsWorld.gravity = CGVectorMake(0, -1);
+        self.physicsWorld.contactDelegate = self;
     }
     return self;
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
     for (UITouch *touch in touches) {
-        //[self ropeSwing];
         touchBeganTime = touch.timestamp;
         touchBeganPoint = [touch locationInNode:self];
     }
@@ -52,12 +61,18 @@ static const CGFloat k_swipeToYVelocityConversion = 0.01;
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     for (UITouch *touch in touches) {
-        NSTimeInterval touchEndTime = touch.timestamp;
         CGPoint touchEndPoint = [touch locationInNode:self];
-        CGFloat xVelocity = k_swipeToXVelocityConversion * (touchEndPoint.x - touchBeganPoint.x) / (touchEndTime - touchBeganTime);
-        CGFloat yVelocity = k_swipeToYVelocityConversion * (touchEndPoint.y - touchBeganPoint.y) / (touchEndTime - touchBeganTime);
-        CGVector swipeVelocity = CGVectorMake(xVelocity, yVelocity);
-        [self ropeSwingWithSwipeVelocity:swipeVelocity];
+        
+        if (fabsf(touchEndPoint.x - touchBeganPoint.x) < 100 && fabsf(touchEndPoint.y - touchBeganPoint.y) < 100) {
+            [self monkeyReleaseRope];
+        } else {
+            NSTimeInterval touchEndTime = touch.timestamp;
+            CGFloat xVelocity = k_swipeToXVelocityConversion * (touchEndPoint.x - touchBeganPoint.x) / (touchEndTime - touchBeganTime);
+            CGFloat yVelocity = k_swipeToYVelocityConversion * (touchEndPoint.y - touchBeganPoint.y) / (touchEndTime - touchBeganTime);
+            CGVector swipeVelocity = CGVectorMake(xVelocity, yVelocity);
+            [self monkeySwingWithSwipeVelocity:swipeVelocity];
+        }
+
     }
 }
 
@@ -74,49 +89,39 @@ static const CGFloat k_swipeToYVelocityConversion = 0.01;
     skyBackground.name = @"skyBackground";
     [self addChild:skyBackground];
     
-    // Add a few trees
-    SKSpriteNode *tree1 = [SKSpriteNode spriteNodeWithImageNamed:@"Standard Tree"];
-    tree1.position = CGPointMake(farLeftSide.x + sceneWidth/11, 0);
-    [self addChild:tree1];
-    
-    SKSpriteNode *tree2 = [SKSpriteNode spriteNodeWithImageNamed:@"TreeStandard"];
-    tree2.position = CGPointMake(farLeftSide.x + sceneWidth/5, 0);
-    [self addChild:tree2];
-
-    SKSpriteNode *tree3 = [SKSpriteNode spriteNodeWithImageNamed:@"TreeMedium3"];
-    tree3.position = CGPointMake(farLeftSide.x + sceneWidth/3, 20);
-    [self addChild:tree3];
-    
-    SKSpriteNode *tree4 = [SKSpriteNode spriteNodeWithImageNamed:@"TreeSmall"];
-    tree4.position = CGPointMake(farLeftSide.x + sceneWidth/2, -15);
-    [self addChild:tree4];
-    
-    SKSpriteNode *tree5 = [SKSpriteNode spriteNodeWithImageNamed:@"TreeMedium2"];
-    tree5.position = CGPointMake(farRightSide.x - sceneWidth/3, 0);
-    [self addChild:tree5];
-
-    SKSpriteNode *tree6 = [SKSpriteNode spriteNodeWithImageNamed:@"TreeMedium1"];
-    tree6.position = CGPointMake(farRightSide.x - sceneWidth/3.5, 30);
-    [self addChild:tree6];
-    
-    SKSpriteNode *tree7 = [SKSpriteNode spriteNodeWithImageNamed:@"TreeBig"];
-    tree7.position = CGPointMake(farRightSide.x - 5, 50);
-    [self addChild:tree7];
+    // Add trees
+    for (int i = 0; i < treeDensity; i++) {
+        // Randomly select which tree image will be used
+        int randomTreeIndex = (arc4random() % (5 - 1 + 1)) + 1;
+        SKSpriteNode *treeNode = [SKSpriteNode spriteNodeWithImageNamed:[NSString stringWithFormat:@"%@%i", @"Tree", randomTreeIndex]];
+        treeNode.anchorPoint = CGPointMake(0.5, 0);
+        treeNode.position = CGPointMake(farLeftSide.x + i/(treeDensity - 1.0) * sceneWidth, farBottomSide.y);
+        treeNode.name = [NSString stringWithFormat:@"%@%i", @"tree", i];
+        [self addChild:treeNode];
+        
+        // Randomly decide if new bush will go in front of or behind last tree
+        int randomIntegerBOOL = (arc4random() % (1 - 0 + 1));
+        if (i > 0 && randomIntegerBOOL == 1) {
+            treeNode.zPosition = 99;
+        } else {
+            treeNode.zPosition = 98;
+        }
+    }
     
     // Add bushes
-    for (int i = 0; i < 14; i++) {
+    for (int i = 0; i < bushDensity; i++) {
         // Randomly select which bush image will be used
         int randomBushIndex = (arc4random() % (5 - 1 + 1)) + 1;
-        SKSpriteNode *bush = [SKSpriteNode spriteNodeWithImageNamed:[NSString stringWithFormat:@"%@%i", @"Bush", randomBushIndex]];
-        bush.position = CGPointMake(farLeftSide.x + i/13.0 * sceneWidth, farBottomSide.y + bush.size.height * 0.4);
-        [self addChild:bush];
+        SKSpriteNode *bushNode = [SKSpriteNode spriteNodeWithImageNamed:[NSString stringWithFormat:@"%@%i", @"Bush", randomBushIndex]];
+        bushNode.position = CGPointMake(farLeftSide.x + i/(bushDensity - 1.0) * sceneWidth, farBottomSide.y + bushNode.size.height * 0.4);
+        [self addChild:bushNode];
         
         // Randomly decide if new bush will go in front of or behind last bush
         int randomIntegerBOOL = (arc4random() % (1 - 0 + 1));
         if (i > 0 && randomIntegerBOOL == 1) {
-            bush.zPosition = 101;
+            bushNode.zPosition = 101;
         } else {
-            bush.zPosition = 100;
+            bushNode.zPosition = 100;
         }
     }
 }
@@ -124,58 +129,121 @@ static const CGFloat k_swipeToYVelocityConversion = 0.01;
 - (void)addFire
 {
     NSString *firePath = [[NSBundle mainBundle] pathForResource:@"ParticleFire" ofType:@"sks"];
-    SKEmitterNode *fireNode = [NSKeyedUnarchiver unarchiveObjectWithFile:firePath];
-    fireNode.position = CGPointMake(farLeftSide.x, farBottomSide.y);
-    [self addChild:fireNode];
-    fireNode.zPosition = 102;
+    SKEmitterNode *fireEmitterNode = [NSKeyedUnarchiver unarchiveObjectWithFile:firePath];
+    fireEmitterNode.position = CGPointMake(farLeftSide.x, farBottomSide.y);
+    [self addChild:fireEmitterNode];
+    fireEmitterNode.zPosition = 105;
+    fireEmitterNode.position = CGPointMake(farLeftSide.x + sceneWidth/2, farBottomSide.y);
+    fireEmitterNode.particlePositionRange = CGVectorMake(0, 0);
+    fireTimer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(fireUpdate:) userInfo:fireEmitterNode repeats:YES];
 }
 
+- (void)fireUpdate:(NSTimer *)timer
+{
+    SKEmitterNode *fireEmitterNode = [timer userInfo];
+    
+    // Initially expand fire from middle outward
+    if (fireEmitterNode.particlePositionRange.dx < sceneWidth) {
+        fireEmitterNode.particlePositionRange = CGVectorMake(fireEmitterNode.particlePositionRange.dx + 100 , 2);
+        fireEmitterNode.particleBirthRate = fireEmitterNode.particleBirthRate + 80; // Hold the intensity roughly constant during expansion
+    }
+    
+    // Make fire faster and higher
+    [fireEmitterNode setParticleSpeed:fireEmitterNode.speed + 1];
+    [fireEmitterNode setParticleLifetime:fireEmitterNode.particleLifetime + 1];
+    if (fireEmitterNode.particleBirthRate < 3000) {
+        fireEmitterNode.particleBirthRate = fireEmitterNode.particleBirthRate + 5;
+    }
+}
+
+// Every tree that is taller than he sceneHeight gets a rope
 - (void)addRopes
 {
-    SKPhysicsBody *ropeSegment1PhysicsBody, *ropeSegment2PhysicsBody;
-    for (int i = 0; i < 15; i = i + 2) {
-        // Add first segment image
-        SKSpriteNode *ropeSegment1 = [SKSpriteNode spriteNodeWithImageNamed:@"Rope Segment"];
-        ropeSegment1.position = CGPointMake(farLeftSide.x + sceneWidth/3, farTopSide.y - i * ropeSegment1.size.height * 0.9);
-        ropeSegment1.name = [NSString stringWithFormat:@"%@%i", @"rope", i];
-        [self addChild:ropeSegment1];
-        
-        // Add first segment physics
-        ropeSegment1PhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ropeSegment1.size];
-        ropeSegment1.physicsBody = ropeSegment1PhysicsBody;
-        ropeSegment1.physicsBody.density = 3;
-        ropeSegment1.physicsBody.linearDamping = 0.8;
-        if (i == 0) {
-            ropeSegment1.physicsBody.dynamic = NO;
+    int ropeNumber = 0;
+    for (SKNode *node in self.children) {
+        if ([node isKindOfClass:SKSpriteNode.class] && [node.name rangeOfString:@"tree"].location != NSNotFound) {
+            SKSpriteNode *treeNode = (SKSpriteNode *)node;
+            if (treeNode.size.height > sceneHeight) {
+                
+                // Make a full rope node to hold all the segments in a single object
+                SKNode *fullRopeNode = [[SKNode alloc] init];
+                fullRopeNode.name = [NSString stringWithFormat:@"%@%i", @"fullRope", ropeNumber];
+                [self addChild:fullRopeNode];
+                
+                // Make a bunch of rope segments
+                SKPhysicsBody *ropeSegment1PhysicsBody, *ropeSegment2PhysicsBody;
+                for (int i = 0; i < 15; i = i + 2) {
+                    // Add first segment image
+                    SKSpriteNode *ropeSegment1 = [SKSpriteNode spriteNodeWithImageNamed:@"Rope Segment"];
+                    ropeSegment1.position = CGPointMake(treeNode.position.x, farTopSide.y - i * ropeSegment1.size.height * 0.9);
+                    ropeSegment1.name = [NSString stringWithFormat:@"%@%i", @"rope", i];
+                    [fullRopeNode addChild:ropeSegment1];
+                    ropeSegment1.zPosition = 110;
+                    
+                    // Add first segment physics
+                    ropeSegment1PhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ropeSegment1.size];
+                    ropeSegment1.physicsBody = ropeSegment1PhysicsBody;
+                    ropeSegment1.physicsBody.density = 3;
+                    ropeSegment1.physicsBody.linearDamping = 0.8;
+                    if (i == 0) {
+                        ropeSegment1.physicsBody.dynamic = NO;
+                    }
+                    ropeSegment1.physicsBody.categoryBitMask = ropeCategory;
+                    ropeSegment1.physicsBody.contactTestBitMask = monkeyCategory;
+                    
+                    // Add joint between new ropeSegment1 and previous ropeSegment2
+                    if (i > 0) {
+                        SKPhysicsBody *previousBottomRopeSegmentPhysicsBody = ropeSegment2PhysicsBody;
+                        SKPhysicsJointSliding *jointPin = [SKPhysicsJointSliding jointWithBodyA:ropeSegment1.physicsBody bodyB:previousBottomRopeSegmentPhysicsBody anchor:CGPointMake(ropeSegment1.position.x, ropeSegment1.position.y + ropeSegment1.size.height) axis:CGVectorMake(1, 0)];
+                        jointPin.shouldEnableLimits = YES;
+                        [self.physicsWorld addJoint:jointPin];
+                    }
+                    
+                    // Add new ropeSegment
+                    SKSpriteNode *ropeSegment2 = [SKSpriteNode spriteNodeWithImageNamed:@"Rope Segment"];
+                    ropeSegment2.position = CGPointMake(ropeSegment1.position.x, farTopSide.y - (i + 1) * ropeSegment2.size.height * 0.9);
+                    ropeSegment2.name = [NSString stringWithFormat:@"%@%i", @"rope", i + 1];
+                    [fullRopeNode addChild:ropeSegment2];
+                    ropeSegment2.zPosition = 110;
+                    
+                    // Add physics to segments
+                    ropeSegment2PhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ropeSegment2.size];
+                    ropeSegment2.physicsBody = ropeSegment2PhysicsBody;
+                    ropeSegment2.physicsBody.density = 3;
+                    ropeSegment2.physicsBody.linearDamping = 0.8;
+                    ropeSegment2.physicsBody.categoryBitMask = ropeCategory;
+                    ropeSegment2.physicsBody.contactTestBitMask = monkeyCategory;
+                    
+                    // Add joints between segments
+                    SKPhysicsJointPin *jointPin = [SKPhysicsJointPin jointWithBodyA:ropeSegment1.physicsBody bodyB:ropeSegment2.physicsBody anchor:CGPointMake(ropeSegment1.position.x, ropeSegment1.position.y - ropeSegment1.size.height)];
+                    jointPin.upperAngleLimit = M_PI/4;
+                    jointPin.lowerAngleLimit = -M_PI/4;
+                    jointPin.shouldEnableLimits = YES;
+                    [self.physicsWorld addJoint:jointPin];
+                }
+                ropeNumber++;
+            }
         }
-        
-        // Add joint between new ropeSegment1 and previous ropeSegment2
-        if (i > 0) {
-            SKPhysicsBody *previousBottomRopeSegmentPhysicsBody = ropeSegment2PhysicsBody;
-            SKPhysicsJointSliding *jointPin = [SKPhysicsJointSliding jointWithBodyA:ropeSegment1PhysicsBody bodyB:previousBottomRopeSegmentPhysicsBody anchor:CGPointMake(ropeSegment1.position.x, ropeSegment1.position.y + ropeSegment1.size.height) axis:CGVectorMake(1, 0)];
-            jointPin.shouldEnableLimits = YES;
-            [self.physicsWorld addJoint:jointPin];
-        }
-        
-        // Add new ropeSegment
-        SKSpriteNode *ropeSegment2 = [SKSpriteNode spriteNodeWithImageNamed:@"Rope Segment"];
-        ropeSegment2.position = CGPointMake(ropeSegment1.position.x, farTopSide.y - (i + 1) * ropeSegment2.size.height * 0.9);
-        ropeSegment2.name = [NSString stringWithFormat:@"%@%i", @"rope", i + 1];
-        [self addChild:ropeSegment2];
-        
-        // Add physics to segments
-        ropeSegment2PhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:ropeSegment2.size];
-        ropeSegment2.physicsBody = ropeSegment2PhysicsBody;
-        ropeSegment2.physicsBody.density = 3;
-        ropeSegment2.physicsBody.linearDamping = 0.8;
-        
-        // Add joints between segments
-        SKPhysicsJointPin *jointPin = [SKPhysicsJointPin jointWithBodyA:ropeSegment1PhysicsBody bodyB:ropeSegment2PhysicsBody anchor:CGPointMake(ropeSegment1.position.x, ropeSegment1.position.y - ropeSegment1.size.height)];
-        jointPin.upperAngleLimit = M_PI/4;
-        jointPin.lowerAngleLimit = -M_PI/4;
-        jointPin.shouldEnableLimits = YES;
-        [self.physicsWorld addJoint:jointPin];
     }
+}
+
+- (void)addMonkey
+{
+    SKSpriteNode *monkeySpriteNode = [SKSpriteNode spriteNodeWithImageNamed:@"Monkey"];
+    monkeySpriteNode.position = CGPointMake(farLeftSide.x + 30, farTopSide.y - 50);
+    monkeySpriteNode.zPosition = 104;
+    monkeySpriteNode.name = @"monkey";
+    [self addChild:monkeySpriteNode];
+    
+    // Baisic properties
+    monkeySpriteNode.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:monkeySpriteNode.size];
+    monkeySpriteNode.physicsBody.mass = 10;
+    monkeySpriteNode.physicsBody.velocity = CGVectorMake(100, 0);
+    
+    // Collision properties
+    monkeySpriteNode.physicsBody.categoryBitMask = monkeyCategory;
+    monkeySpriteNode.physicsBody.contactTestBitMask = ropeCategory;
+    monkeySpriteNode.physicsBody.usesPreciseCollisionDetection = YES;
 }
 
 #pragma mark - Convenience methods
@@ -194,8 +262,78 @@ static const CGFloat k_swipeToYVelocityConversion = 0.01;
 
 - (void)ropeSwingWithSwipeVelocity:(CGVector)swipeVelocity
 {
-    SKNode *ropeSegment = [self childNodeWithName:@"rope9"];
-    [ropeSegment.physicsBody applyImpulse:swipeVelocity];
+    // Determine which rope segment to apply impulse to
+    CGPoint rayStart = touchBeganPoint;
+    CGPoint rayEnd = CGPointMake(farRightSide.x, touchBeganPoint.y);
+    
+    SKPhysicsBody *bodyAlongRay = [self.physicsWorld bodyAlongRayStart:rayStart end:rayEnd];
+    if (bodyAlongRay && [bodyAlongRay.node.name rangeOfString:@"rope"].location != NSNotFound) {
+        [bodyAlongRay applyImpulse:swipeVelocity];
+    }
+}
+
+- (void)monkeySwingWithSwipeVelocity:(CGVector)swipeVelocity
+{
+    SKPhysicsBody *monkeyPhysicsBody = [self childNodeWithName:@"monkey"].physicsBody;
+    if (monkeyPhysicsBody.joints.count != 0) {
+        [monkeyPhysicsBody applyImpulse:swipeVelocity];
+    }
+}
+
+- (void)monkeyReleaseRope
+{
+    // Make the rope and monkey contactless to allow monkey to move through
+    for (SKNode *node in self.children) {
+        if ([node.name isEqualToString:monkeyOnRopeWithName]) {
+            for (SKNode *ropeSegmentNode in node.children) {
+                [ropeSegmentNode.physicsBody setContactTestBitMask:0];
+                [ropeSegmentNode.physicsBody setCollisionBitMask:0];
+            }
+        }
+    }
+    SKPhysicsBody *monkeyPhysicsBody = [self childNodeWithName:@"monkey"].physicsBody;
+    monkeyPhysicsBody.collisionBitMask = 0;
+    monkeyPhysicsBody.contactTestBitMask = 0;
+    
+    // Remove joint between monkey and rope
+    for (SKPhysicsJoint *joint in monkeyPhysicsBody.joints) {
+        [self.physicsWorld removeJoint:joint];
+    }
+    
+    // Add a jumping impulse to the monkey
+    [monkeyPhysicsBody applyImpulse:CGVectorMake(1000, 500)];
+}
+
+#pragma mark - Collision response methods
+
+- (void)didBeginContact:(SKPhysicsContact *)contact
+{
+    // Sort which bodies are which
+    SKPhysicsBody *firstBody, *secondBody;
+    if (contact.bodyA.categoryBitMask > contact.bodyB.categoryBitMask) {
+        firstBody = contact.bodyA;
+        secondBody = contact.bodyB;
+    }
+    else {
+        firstBody = contact.bodyB;
+        secondBody = contact.bodyA;
+    }
+    
+    // Verify that the two bodies were the monkey and rope, then handle collision
+    if ((firstBody.categoryBitMask & ropeCategory) != 0 && (secondBody.categoryBitMask & monkeyCategory) != 0)
+    {
+        [self monkey:secondBody didCollideWithRope:firstBody];
+    }
+}
+
+- (void)monkey:(SKPhysicsBody *)monkeyPhysicsBody didCollideWithRope:(SKPhysicsBody *)ropePhysicsBody
+{
+    SKPhysicsJointPin *jointPin = [SKPhysicsJointPin jointWithBodyA:monkeyPhysicsBody bodyB:ropePhysicsBody anchor:ropePhysicsBody.node.position];
+    jointPin.upperAngleLimit = M_PI/4;
+    jointPin.shouldEnableLimits = YES;
+    [self.physicsWorld addJoint:jointPin];
+    
+    monkeyOnRopeWithName = ropePhysicsBody.node.parent.name;
 }
 
 @end
